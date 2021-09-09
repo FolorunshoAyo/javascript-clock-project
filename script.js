c6d522c46ceae829926fcd026d9a9d63759f575b
FolorunshoAyo <folushoayomide11@gmail.com>
2021-09-09 23:46:52 +0100

    added comments to the script.js file

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 (function(){
     setInterval(setClock, 1000);
 
+    // storing element nodes in their respective variables
     const secondHand = document.querySelector("[data-second-hand]");
     const minuteHand = document.querySelector("[data-minute-hand]");
     const hourHand = document.querySelector("[data-hour-hand]");
 
     function setClock(){
+        // creating a new instance of a date object
         const currentDate = new Date();
+
+        //calculating all the necessary ratios for each time elements
         const secondsRatio = (currentDate.getSeconds()) / 60;
         const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
         const hourRatio = currentDate.getHours() / 24;
 
+        //calling a function that sets the rotation of all the arms
         setRotation(secondHand, secondsRatio);
         setRotation(minuteHand, minutesRatio);
         setRotation(hourHand, hourRatio);
@@ -20,5 +25,6 @@
         element.style.setProperty("--rotation", timingRatio * 360);
     }
 
+    // This function gets called before the setTimeout runs the same function to avoid delay after refresh. 
     setClock();
 }());
